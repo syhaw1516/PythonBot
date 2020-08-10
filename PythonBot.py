0ee7419c5bf37ca826ade0ea621252add736ff32
syhaw1516 <64174814+syhaw1516@users.noreply.github.com>
2020-08-10 22:42:29 +0900

    Update PythonBot.py

--- PythonBot.py
+++ PythonBot.py
@@ -17,7 +17,7 @@ async def on_message(message):
         return
 
     if message.content.startswith('!도움말'):
-        await message.channel.send("```Python Boy 명령어\n\n 1. !도움말 - 명령어를 보여줍니다.\n\n 2. !밥 - 음식중 아무거나 나옵니다.\n\n 3. !ping - 서버 핑을 확인해줍니다.\n\n 4. !파이썬 - 개발자의 한탄을 들을 수 있습니다. \n\n```")
+        await message.channel.send("```Python Boy 명령어\n\n 1. !도움말 - 명령어를 보여줍니다.\n\n 2. !밥 - 음식중 아무거나 나옵니다.\n\n 3. !ping - 서버 핑을 확인해줍니다.\n\n 4. !파이썬 - 개발자의 한탄을 들을 수 있습니다. \n\n 5. !오늘배그 - 오늘의 배그 각을 알려줍니다.(그냥 만듬 심심해서) \n\n 6. !오늘서든 - 오늘의 서든 각을 알려줍니다.(그냥 만듬 심심해서) ```")
 
     if message.content.startswith('!밥'):
         i = random.randint(0,23)
@@ -28,7 +28,7 @@ async def on_message(message):
         await message.channel.send(f"```Pong! {round(l*1000)}ms 입니다.```")
 
     if message.content.startswith('!파이썬'):
-        i = random.randint(1,3)
+        i = random.randint(1,4)
 
         if i == 1:
             await message.channel.send("```하 만들기 힘들다...ㅎ..ㅎ.```")
@@ -38,6 +38,25 @@ async def on_message(message):
 
         if i == 3:
             await message.channel.send("```하 그냥 좀 되면 안돼??? 왜이래...```")
+        
+        if i == 4:
+            await message.channel.send("```많이 사용해주면... 좀 많이 기쁠지도..(O.O)```")
+
+    if message.content.startswith('!오늘배그'):
+        i =  random.randint(1,2)
+        if i == 1:
+            await message.channel.send(f'```나쁘지않아 각이야 가즈아ㅏㅏㅏㅏㅏㅏㅏㅏ```')
+
+        if i == 2:
+            await message.channel.send(f'```음 오늘은 좀 아닌듯 그냥 자자 에휴...```')
+
+    if message.content.startswith('!오늘서든'):
+        i =  random.randint(1,2)
+        if i == 1:
+            await message.channel.send(f'```나쁘지않아 각이야 가즈아ㅏㅏㅏㅏㅏㅏㅏㅏ```')
+
+        if i == 2:
+            await message.channel.send(f'```음 오늘은 좀 아닌듯 그냥 자자 에휴...```')
 
 access_token = os.environ["BOT_TOKEN"]
 bot.run(access_token)
